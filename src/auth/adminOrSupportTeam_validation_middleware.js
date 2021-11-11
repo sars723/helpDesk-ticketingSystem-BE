e0931b728b88bd69e8ac1ecd39e3fdca209a16f1
sars723 <sarasalomonn@gmail.com>
2021-11-11 21:33:44 +0100

    comment

--- src/auth/adminOrSupportTeam_validation_middleware.js
+++ src/auth/adminOrSupportTeam_validation_middleware.js
@@ -1,13 +1,13 @@
 import createHttpError from "http-errors";
 
 export const onlyAdminAllowedRoute = (req, res, next) => {
-  console.log("role", req.user.role);
+  /* console.log("role", req.user.role); */
   if (req.user.role === "admin") {
     next();
   } else next(createHttpError(403, "Admin only! "));
 };
 export const onlyAdminAndSupportTeamAllowedRoute = (req, res, next) => {
-  console.log("role", req.user);
+  /*  console.log("role", req.user); */
   if (req.user.role === "admin" || req.user.role === "support-team") {
     next();
   } else next(createHttpError(403, "Admin and support-team only! "));
